217afe5446606930af0a3108011310d40f38a947
unknown <ashish11to12@gmail.com>
2026-03-02 13:12:12 +0530

    added context

--- AI_Chatapp/chatbot.py
+++ AI_Chatapp/chatbot.py
@@ -1,12 +1,22 @@
 from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
 llm=HuggingFacePipeline.from_model_id(model_id="Qwen/Qwen2-0.5B-Instruct",
-                                      task="text-generation")
+                                      task="text-generation"
+                                      ,
+                                       pipeline_kwargs=dict(
+        max_new_tokens=150,
+        return_full_text=False   
+    )
+                     )
 model=ChatHuggingFace(llm=llm)
+context=[]
 while True:
     user_input=input('you: ')
+    context.append(user_input)
     if user_input=='exit':
         break;
     else:
-        result=model.invoke(user_input) 
+        result=model.invoke(context) 
         result=result
-        print('AI: ',result.content)
+        response = result.content
+        context.append(response)
+        print("AI:", response)
