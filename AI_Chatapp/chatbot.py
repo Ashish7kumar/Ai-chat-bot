2ec4d189d05f5412fc16208c6107685bf89aed21
unknown <ashish11to12@gmail.com>
2026-03-02 13:34:51 +0530

    replaced qwen with gemni

--- AI_Chatapp/chatbot.py
+++ AI_Chatapp/chatbot.py
@@ -1,18 +1,21 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFacePipeline
-llm=HuggingFacePipeline.from_model_id(model_id="Qwen/Qwen2-0.5B-Instruct",
-                                      task="text-generation"
-                                      ,
-                                       pipeline_kwargs=dict(
-        max_new_tokens=150,
-        return_full_text=False   
-    )
-                     )
-model=ChatHuggingFace(llm=llm)
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+     api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.2
+)
 context=[]
 while True:
     user_input=input('you: ')
     context.append(user_input)
     if user_input=='exit':
+        print(str(context))
         break;
     else:
         result=model.invoke(context) 
